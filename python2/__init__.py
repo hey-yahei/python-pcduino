d1a3c4a2d19fe09a564a3477cf1aeb038aed3e39
hey-yahei <31240114+hey-yahei@users.noreply.github.com>
2018-05-15 23:59:26 +0800

    Initial commit

---
+++ python2/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from pcduino.gpio import *
+from pcduino.adc import analog_read
+from pcduino.pwm import *
+from pcduino.interrupts import *
+from pcduino.myserial import Serial
